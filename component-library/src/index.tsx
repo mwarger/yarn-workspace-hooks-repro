76ef62d07987b20eda6e958233ff4a9b1dc37ce1
Mat Warger <Mat_Warger@AmericanCentury.com>
2019-03-14 13:36:39 -0500

    updated with initially commented out

--- component-library/src/index.tsx
+++ component-library/src/index.tsx
@@ -3,9 +3,10 @@ import * as React from "react";
 import { Child } from "./Child";
 
 export const App: React.FC<{ message?: string }> = ({ message = "hello" }) => {
-  React.useEffect(() => {
-    console.log("hi");
-  }, []);
+  // TODO: uncomment this to see the error
+  // React.useEffect(() => {
+  //   console.log("hi");
+  // }, []);
 
   return <Child>{message}</Child>;
 };
--- example-demo/src/App.js
+++ example-demo/src/App.js
@@ -7,8 +7,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <ChildApp />
         <header className="App-header">
+          <ChildApp message="This is the child app component" />
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
